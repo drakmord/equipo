ac9d95038a010366de0fca866cc03bf7a2918237
drakmord <stiff_flores_18@hotmail.com>
2014-12-13 11:55:43 -0500

    Create equipo.blade.php

---
+++ equipo.blade.php
@@ -0,0 +1,448 @@
+@extends('layouts.master')
+@section('Cabecera')
+<div class="container">
+				<div class="navbar-brand">
+					<!-- COMPANY LOGO -->
+					<a href="index.html">
+						<img src="{{ asset('assets/img/logo/logo.png') }}" alt="esDOTA.com" class="img-responsive" height="30">
+					</a>
+					<!-- /COMPANY LOGO -->
+					<!-- TEAM STATUS FOR MOBILE -->
+					<div class="visible-xs">
+						<a href="#" class="team-status-toggle switcher btn dropdown-toggle">
+							<i class="fa fa-users"></i>
+						</a>
+					</div>
+					<!-- /TEAM STATUS FOR MOBILE -->
+					<!-- SIDEBAR COLLAPSE -->
+					<div id="sidebar-collapse" class="sidebar-collapse btn">
+						<i class="fa fa-bars" 
+							data-icon1="fa fa-bars" 
+							data-icon2="fa fa-bars" ></i>
+					</div>
+					<!-- /SIDEBAR COLLAPSE -->
+				</div>
+				<!-- BEGIN TOP NAVIGATION MENU -->					
+				<ul class="nav navbar-nav pull-right">
+					<!-- BEGIN NOTIFICATION DROPDOWN -->	
+					<li class="dropdown" id="header-notification">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<i class="fa fa-bell"></i>
+							<span class="badge">7</span>
+							
+						</a>
+						<ul class="dropdown-menu notification">
+							<li class="dropdown-title">
+								<span><i class="fa fa-bell"></i> 7 Notifications</span>
+							</li>
+							<li>
+								<a href="#">
+									<span class="label label-success"><i class="fa fa-user"></i></span>
+									<span class="body">
+										<span class="message">5 users online. </span>
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>Just now</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="label label-primary"><i class="fa fa-comment"></i></span>
+									<span class="body">
+										<span class="message">Martin commented.</span>
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>19 mins</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="label label-warning"><i class="fa fa-lock"></i></span>
+									<span class="body">
+										<span class="message">DW1 server locked.</span>
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>32 mins</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="label label-info"><i class="fa fa-twitter"></i></span>
+									<span class="body">
+										<span class="message">Twitter connected.</span>
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>55 mins</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="label label-danger"><i class="fa fa-heart"></i></span>
+									<span class="body">
+										<span class="message">Jane liked. </span>
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>2 hrs</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="label label-warning"><i class="fa fa-exclamation-triangle"></i></span>
+									<span class="body">
+										<span class="message">Database overload.</span>
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>6 hrs</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="footer">
+								<a href="#">See all notifications <i class="fa fa-arrow-circle-right"></i></a>
+							</li>
+						</ul>
+					</li>
+					<!-- END NOTIFICATION DROPDOWN -->
+					<!-- BEGIN INBOX DROPDOWN -->
+					<li class="dropdown" id="header-message">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<i class="fa fa-envelope"></i>
+						<span class="badge">3</span>
+						</a>
+						<ul class="dropdown-menu inbox">
+							<li class="dropdown-title">
+								<span><i class="fa fa-envelope-o"></i> 3 Messages</span>
+								<span class="compose pull-right tip-right" title="Compose message"><i class="fa fa-pencil-square-o"></i></span>
+							</li>
+							<li>
+								<a href="#">
+									<img src="img/avatars/avatar2.jpg" alt="" />
+									<span class="body">
+										<span class="from">Jane Doe</span>
+										<span class="message">
+										Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse mole ...
+										</span> 
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>Just Now</span>
+										</span>
+									</span>
+									 
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="img/avatars/avatar1.jpg" alt="" />
+									<span class="body">
+										<span class="from">Vince Pelt</span>
+										<span class="message">
+										Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse mole ...
+										</span> 
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>15 min ago</span>
+										</span>
+									</span>
+									 
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="img/avatars/avatar8.jpg" alt="" />
+									<span class="body">
+										<span class="from">Debby Doe</span>
+										<span class="message">
+										Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse mole ...
+										</span> 
+										<span class="time">
+											<i class="fa fa-clock-o"></i>
+											<span>2 hours ago</span>
+										</span>
+									</span>
+									 
+								</a>
+							</li>
+							<li class="footer">
+								<a href="#">See all messages <i class="fa fa-arrow-circle-right"></i></a>
+							</li>
+						</ul>
+					</li>
+					<!-- END INBOX DROPDOWN -->
+					<!-- BEGIN TODO DROPDOWN -->
+					<li class="dropdown" id="header-tasks">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<i class="fa fa-tasks"></i>
+						<span class="badge">3</span>
+						</a>
+						<ul class="dropdown-menu tasks">
+							<li class="dropdown-title">
+								<span><i class="fa fa-check"></i> 6 tasks in progress</span>
+							</li>
+							<li>
+								<a href="#">
+									<span class="header clearfix">
+										<span class="pull-left">Software Update</span>
+										<span class="pull-right">60%</span>
+									</span>
+									<div class="progress">
+									  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+										<span class="sr-only">60% Complete</span>
+									  </div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="header clearfix">
+										<span class="pull-left">Software Update</span>
+										<span class="pull-right">25%</span>
+									</span>
+									<div class="progress">
+									  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+										<span class="sr-only">25% Complete</span>
+									  </div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="header clearfix">
+										<span class="pull-left">Software Update</span>
+										<span class="pull-right">40%</span>
+									</span>
+									<div class="progress progress-striped">
+									  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+										<span class="sr-only">40% Complete</span>
+									  </div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="header clearfix">
+										<span class="pull-left">Software Update</span>
+										<span class="pull-right">70%</span>
+									</span>
+									<div class="progress progress-striped active">
+									  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+										<span class="sr-only">70% Complete</span>
+									  </div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="header clearfix">
+										<span class="pull-left">Software Update</span>
+										<span class="pull-right">65%</span>
+									</span>
+									<div class="progress">
+									  <div class="progress-bar progress-bar-success" style="width: 35%">
+										<span class="sr-only">35% Complete (success)</span>
+									  </div>
+									  <div class="progress-bar progress-bar-warning" style="width: 20%">
+										<span class="sr-only">20% Complete (warning)</span>
+									  </div>
+									  <div class="progress-bar progress-bar-danger" style="width: 10%">
+										<span class="sr-only">10% Complete (danger)</span>
+									  </div>
+									</div>
+								</a>
+							</li>
+							<li class="footer">
+								<a href="#">See all tasks <i class="fa fa-arrow-circle-right"></i></a>
+							</li>
+						</ul>
+					</li>
+					<!-- END TODO DROPDOWN -->
+					<!-- BEGIN USER LOGIN DROPDOWN -->
+					<li class="dropdown user" id="header-user">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<img alt="" src="{{ asset('assets/img/avatars/avatar3.jpg') }}" />
+							<span class="username">John Doe</span>
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="#"><i class="fa fa-user"></i> My Profile</a></li>
+							<li><a href="#"><i class="fa fa-cog"></i> Account Settings</a></li>
+							<li><a href="#"><i class="fa fa-eye"></i> Privacy Settings</a></li>
+							<li><a href="login.html"><i class="fa fa-power-off"></i> Log Out</a></li>
+						</ul>
+					</li>
+					<!-- END USER LOGIN DROPDOWN -->
+				</ul>
+				<!-- END TOP NAVIGATION MENU -->
+		</div>
+@stop
+
+@section('Menu')
+@parent
+@stop
+
+@section('texto')
+<div class="col-sm-12">
+								<div class="page-header">
+									<!-- STYLER -->
+									
+									<!-- /STYLER -->
+									<!-- BREADCRUMBS -->
+									<ul class="breadcrumb">
+										<li>
+											<i class="fa fa-home"></i>
+											<a href="index.html">Home</a>
+										</li>
+										<li>
+											<a href="#">Form Elements</a>
+										</li>
+										<li>Heroes</li>
+									</ul>
+									<!-- /BREADCRUMBS -->
+									<div class="clearfix">
+										<h3 class="content-title pull-left">Heroes</h3>
+									</div>
+									<div class="description">Ingrese todos los datos</div>
+								</div>
+							</div>
+@stop												
+@section('cuerpo')
+									<div class="col-md-6">
+										<!-- BOX -->
+										<div class="box border red">
+											<div class="box-title">
+												<h4><i class="fa fa-bars"></i></h4>
+												<div class="tools">
+													<a href="#box-config" data-toggle="modal" class="config">
+														<i class="fa fa-cog"></i>
+													</a>
+													<a href="javascript:;" class="reload">
+														<i class="fa fa-refresh"></i>
+													</a>
+													<a href="javascript:;" class="collapse">
+														<i class="fa fa-chevron-up"></i>
+													</a>
+													<a href="javascript:;" class="remove">
+														<i class="fa fa-times"></i>
+													</a>
+												</div>
+											</div>
+											<div class="box-body">
+												
+												{{Form::open(['route' => 'equipo', 'method' => 'POST', 'role' => 'form']) }}
+													<div class="form-group">
+														 
+													{{ Form::label('nombre', 'Nombre del equipo') }}
+        											{{ Form::text('nombre', null, ['class' => 'form-control']) }}
+        											{{ Form::label('subtitulo', 'subtitulo del equipo') }}
+        											{{ Form::text('subtitulo', null, ['class' => 'form-control']) }}
+        											{{ Form::label('soloadmin', 'Solo Admin Master') }}
+        											{{ Form::text('soloadmin', null, ['class' => 'form-control']) }}
+        											
+												  </div>
+		
+												
+											</div>
+										</div>
+										<!-- /BOX -->
+									</div>
+
+
+									<div class="col-md-6">
+									<!-- BOX -->
+									<div class="box border blue">
+										<div class="box-title">
+											<h4><i class="fa fa-bars"></i></h4>
+											<div class="tools">
+												<a href="#box-config" data-toggle="modal" class="config">
+													<i class="fa fa-cog"></i>
+												</a>
+												<a href="javascript:;" class="reload">
+													<i class="fa fa-refresh"></i>
+												</a>
+												<a href="javascript:;" class="collapse">
+													<i class="fa fa-chevron-up"></i>
+												</a>
+												<a href="javascript:;" class="remove">
+													<i class="fa fa-times"></i>
+												</a>
+											</div>
+										</div>
+										<div class="box-body big">
+											
+											  <div class="form-group">
+											  	{{ Form::label('precio', 'Precio') }}
+													  <div class="input-group">
+														  <span class="input-group-addon">$</span>
+														  <input type="text" name="precio" class="form-control">
+														  <span class="input-group-addon">.00</span>
+														</div>
+													{{ Form::label('comision', 'Comision') }}
+													<div class="input-group">
+													  <span class="input-group-addon">
+														<input type="checkbox">
+													  </span>
+													  <input type="text" name="comision" class="form-control">
+													</div>
+
+													{{ Form::label('runas', 'Ranuras de Canjes') }}
+													<div class="input-group">
+													  <span class="input-group-addon">
+														<input type="checkbox">
+													  </span>
+													  <input type="text" name="runas" class="form-control">
+													</div>
+													
+													{{ Form::label('idaspectos', 'Aspectos') }}
+													<div class="aspectos_checkbox">
+													@foreach($listaAspectos as $listaAspecto)
+													<input type="radio" value="{{$listaAspecto->idaspecto}}" name="idaspecto">{{ $listaAspecto->nombre }} 
+													   <br>
+													@endforeach
+
+													{{ Form::label('idrareza', 'RAREZA') }}
+													<div class="rareza_checkbox">
+													@foreach($listaRarezas as $Rareza)
+													<input type="radio" value="{{$Rareza->idrareza}}" name="idrareza">{{ $Rareza->nombre }} 
+													   <br>
+													@endforeach
+													</div>
+
+												{{ Form::label('espacio', 'Espacio') }}
+												{{ Form::text('espacio', null, ['class' => 'form-control']) }}	
+
+												{{ Form::label('creado', 'Creado por') }}
+												{{ Form::text('creado', null, ['class' => 'form-control']) }}
+
+												
+												<p>
+												{{ Form::label('lanzado', 'Lanzado') }}
+												{{ Form::text('lanzado', null, ['class' => 'form-control']) }}
+												</p>
+
+												<p>
+												{{ Form::label('descripcion', 'Descripcion') }}
+												{{ Form::text('descripcion', null, ['class' => 'form-control']) }}
+												</p> 
+
+												<p>
+           											 <input type="submit" value="Enviar" class="btn btn-success">
+       											 </p> 
+
+													
+
+												  </div>
+										</div>
+									</div>
+									<!-- /BOX -->
+								</div>
+								{{ Form::close()}}
+@stop
